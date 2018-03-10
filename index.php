eb0e8978734ae684f4551cfe22d0f3b80ca6f26c
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-03-09 20:19:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,99 @@
-<!DOCTYPE html> <html>
+<!DOCTYPE html><html>
 <head>
-<style>
-body {font-family: sans-serif;}
+  <style>
+    body {
+      font-family: sans-serif;
+    }
+    
+    /* can make stuff invisible/not invisible */
+    .hidden{
+      display:none;
+    }
 
-/* means those fonts have no vertical padding */
-h2{
-    line-height:0px;
-}
-font{
-    line-height:0px;
-}
-h3{
-    line-height:0px;
-}
-
-</style>
+  </style>
+  <title>Register</title>
 </head>
 <body>
-<h2>Welcome to the Economy Simulator Beta - Version 0.1.0!</h2>
-<font size="+1">Log in or register below.</font> <br><br><br>
-
-<!-- Log in area -->
-<form>
-	Username: <br>
-	<input type="text" name="username"> <br><br>
-	Password: <br>
-	<input type="text" name="password"> <br><br>
-	<input type="submit" value="Submit">
-    <a href="register.php">Register</a>
-</form>
-
-<!--Release Notes-->
-<br><br><br><br><br><h2>Release Notes</h2>
-<br><h3>Version 0.1.0</h3>
-<p>The first release of the Economy Simulator. This version simple contains the bare elements of the simulator and has will require many adjustments down the road to ensure fairness, a better experience, and an overall better game. The GUI is currently essentially nonexistent, only using default html graphics (AKA, I know it looks disgusting. I promise the full release (1.0) will be bearable). An overview of features in this version is listed below.  </p>
-<ul>
-	<li>This</li>
-	<li>That</li>
-	<li>The Other</li>
-</ul>
-
+  <h2>Register</h2>
+  
+  <!--Enter credentials-->
+  <form method="post" action="">
+    Username:<br><input type="text" name="username" onkeyup="userVal()" required><br><br>
+    Email Address: <br><input type="text" name="email" onkeyup="emailVal()" required><br><br>
+    Password:<br><input type="password" name="password" onkeyup="passVal()" required><br><br>
+    Confirm Password:<br><input type="password" name="passwordconf" onkeyup="passVal()" required><br><br>
+    
+    <input type="submit" value="Submit" disabled name="button1" id="submitButton">
+  </form>
+  
+  <!-- Where it is displayed that passwords do not match.-->
+  <p id="noPassMatch"></p>
 </body>
 </html>
+<script>
+
+  /*Assigns variables to contents of text boxes*/
+  var user = document.getElementsByName("username")[0];
+  var email = document.getElementsByName("email")[0];  
+  var pass = document.getElementsByName("password")[0];
+  var confPass = document.getElementsByName("passwordconf")[0];
+  
+  /*disables submit button by default*/
+  document.getElementById('submitButton').disabled = true;
+
+  /*Hides and shows submit button/error message based on passwords being the same and not being empty*/
+  function passVal() {
+    if (pass.value != confPass.value) {
+      document.getElementById('noPassMatch').innerHTML = "Passwords do not match";
+      document.getElementById('submitButton').disabled = true;
+      document.getElementById("noPassMatch").classList.remove('hidden');
+    } else {
+      document.getElementById("noPassMatch").classList.add('hidden');
+      document.getElementById('submitButton').disabled = false;
+    }
+    if (pass.value.length < 1) {
+      document.getElementById('submitButton').disabled = true;
+    }
+  }
+  
+  /*Hides and shows submit button based on fields being filled in*/
+  function userVal() {
+    if (user.value.length < 1) {
+      document.getElementById('submitButton').disabled = true;
+    } else {
+      passVal()
+    }
+  }
+  function emailVal() {
+    if (email.value.length < 1) {
+      document.getElementById('submitButton').disabled = true;
+    } else {
+      passVal()
+    }
+  }
+
+  
+</script>
+<?php
+
+//when submit button is pressed
+if (isset($_POST['button1']))
+{
+
+  //connect to the database
+  $connect=mysqli_connect('localhost','root','root','econ_data');
+  if(mysqli_connect_errno($connect))
+  {
+    echo 'Failed to connect';
+  }
+
+  //assign variable to contents of text boxes
+  $username=$_POST['username'];
+  $email=$_POST['email'];
+  $password=$_POST['password'];
+
+  //Send variable which equals contents of text boxes to database
+  mysqli_query($connect,"INSERT INTO users(username,password,email) VALUES('$username','$password','$email')");
+
+}
+?>
