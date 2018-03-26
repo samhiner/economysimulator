288793ddb36257a32c4aac2faf1f0343fdaba228
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-03-25 21:33:38 -0400

    Remove PHP

--- registerthankyou.php
+++ registerthankyou.php
@@ -1,28 +1,4 @@
 <!DOCTYPE html><html>
-<body>hey</body>
-</html>
-<?php
-  //defines connecting to database to a variable
-  $connect=mysqli_connect('localhost','root','root','econ_data');
-
-  //checks mysql database connection
-  if(mysqli_connect_errno($connect)) {
-    echo 'Failed to connect';
-  }
-
-  //defines the act of "putting the contents of the text boxes through the data cleaner" to a variable
-  $cleanUsername = dataCleaner($_POST["username"]);
-  $cleanEmail = dataCleaner($_POST["email"]);
-  $cleanPassword = dataCleaner($_POST["password"]);
-    
-  //function to clean data to prevent hacking
-  function dataCleaner($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
-  //Send variable which equals contents of text boxes to database
-  mysqli_query($connect,"INSERT INTO users(username,password,email) VALUES('$cleanUsername','$cleanPassword','$cleanEmail')");
-?>
+<body>
+  <p>Thank you for registering for The Economy Simulator. Click the link provided in the email sent to you within the next 24 hours to verify your registration. If your account is not verified in 24 hours, you will have to restart the registration process.</p>
+</body></html>
